e2b32e1d8fc0c2c004e714a255cfa4053c39c499
kasa.barnabas <kasa.barnabas@encosoft.hu>
2019-06-18 10:32:11 +0200

    Added two DBContext classes.
    Only the users table is supported for now.

---
+++ backend/StrategyGame.Dal/UnderSeaDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace StrategyGame.Dal
+{
+    /// <summary>
+    /// Represents a (writeable) database context towards a UnderSea database.
+    /// </summary>
+    public class UnderSeaDatabase : ReadOnlyUnderSeaDatabase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnderSeaDatabase"/>.
+        /// </summary>
+        /// <param name="options">The <see cref="DbContextOptions{NingyoDatabase}"/> for the database.</param>
+        /// <exception cref="ArgumentNullException">Thrown if an argument was null.</exception>
+        public UnderSeaDatabase(DbContextOptions<UnderSeaDatabase> options)
+            : base(options, false)
+        { }
+    }
+}
