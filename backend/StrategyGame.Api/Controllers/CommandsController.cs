37e2d81a8b748997b98f0c4b0804d4b211a561dd
Péter Koppa <koppa96@windowslive.com>
2019-06-19 17:12:35 +0200

    Adding Commands controller

---
+++ backend/StrategyGame.Api/Controllers/CommandsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StrategyGame.Api.DTO;
+
+namespace StrategyGame.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult<IEnumerable<CommandInfo>>> GetCommandsAsync()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult> AttackTargetAsync([FromBody] CommandInfo command)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(204)]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult> UpdateCommand([FromBody] CommandInfo command)
+        {
+            return Ok();
+        }
+    }
+}
