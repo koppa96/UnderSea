37f9df7f2122a7abac4637221d5d4d36e455375e
kasa.barnabas <kasa.barnabas@encosoft.hu>
2019-06-20 10:57:26 +0200

    Fixed some comments

--- backend/StrategyGame.Model/Entities/AbstractEntity.cs
+++ backend/StrategyGame.Model/Entities/AbstractEntity.cs
@@ -18,13 +18,13 @@ namespace StrategyGame.Model.Entities
         public int Id { get; protected internal set; }
 
         /// <summary>
-        /// Gets the hashcode of this <see cref="INingyoEntity{T}"/>.
+        /// Gets the hashcode of this <see cref="AbstractEntity{T}"/>.
         /// </summary>
         /// <returns>The hashcode.</returns>
         public override int GetHashCode() => Id.GetHashCode();
 
         /// <summary>
-        /// Decides if this instance of <see cref="INingyoEntity{T}"/> is equal to another.
+        /// Decides if this instance of <see cref="AbstractEntity{T}"/> is equal to another.
         /// </summary>
         /// <param name="Other">The other <see cref="object"/>.</param>
         /// <returns>If they were equal.</returns>
