9121604281198ebc29f45a085687eb9f72eac116
Péter Koppa <koppa96@windowslive.com>
2019-06-20 10:37:50 +0200

    Creating interface for UnitService and adding it to the UnitController

--- backend/StrategyGame.Api/Controllers/UnitsController.cs
+++ backend/StrategyGame.Api/Controllers/UnitsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StrategyGame.Bll.DTO;
+using StrategyGame.Bll.Services.Units;
 
 namespace StrategyGame.Api.Controllers
 {
@@ -14,12 +15,19 @@ namespace StrategyGame.Api.Controllers
     [Authorize]
     public class UnitsController : ControllerBase
     {
+        private readonly IUnitService _unitService;
+
+        public UnitsController(IUnitService unitService)
+        {
+            _unitService = unitService;
+        }
+
         [HttpGet]
         [ProducesResponseType(401)]
         [ProducesResponseType(200)]
         public async Task<ActionResult<IEnumerable<UnitInfo>>> GetAllUnitsAsync()
         {
-            return Ok();
+            return Ok(await _unitService.GetUnitInfoAsync(User.Identity.Name));
         }
 
         [HttpPost("{id}/{count}")]
@@ -29,16 +37,60 @@ namespace StrategyGame.Api.Controllers
         [ProducesResponseType(201)]
         public async Task<ActionResult<UnitInfo>> CreateAsync(int id, int count)
         {
-            return Ok();
+            try
+            {
+                return Ok(await _unitService.CreateUnitAsync(User.Identity.Name, id, count));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound(new ProblemDetails
+                {
+                    Status = 404,
+                    Title = "Not Found",
+                    Detail = "No unit type found with the given ID."
+                });
+            }
+            catch (ArgumentException)
+            {
+                return BadRequest(new ProblemDetails
+                {
+                    Status = 400,
+                    Title = "Bad Request",
+                    Detail = "Invalid amount. The amount must be a positive number and you have to have enough money."
+                });
+            }
         }
 
         [HttpDelete("{id}/{count}")]
         [ProducesResponseType(401)]
         [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
         [ProducesResponseType(204)]
         public async Task<ActionResult> DeleteAsync(int id, int count)
         {
-            return Ok();
+            try
+            {
+                await _unitService.DeleteUnitsAsync(User.Identity.Name, id, count);
+                return NoContent();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return NotFound(new ProblemDetails
+                {
+                    Status = 404,
+                    Title = "Not Found",
+                    Detail = "No unit type found with the given ID."
+                });
+            }
+            catch (ArgumentException)
+            {
+                return BadRequest(new ProblemDetails
+                {
+                    Status = 400,
+                    Title = "Bad Request",
+                    Detail = "Invalid amount. The amount must be a positive number and not more than the amount of units you have."
+                });
+            }
         }
     }
 }
--- backend/StrategyGame.Bll/Services/Units/IUnitService.cs
+++ backend/StrategyGame.Bll/Services/Units/IUnitService.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using StrategyGame.Bll.DTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace StrategyGame.Bll.Services.Units
 {
+    /// <summary>
+    /// Service for managing units of a player.
+    /// </summary>
     public interface IUnitService
     {
-        
+        /// <summary>
+        /// Gets UnitInfos for the given player.
+        /// </summary>
+        /// <param name="username">The name of the player</param>
+        /// <returns>An IEnumerable containing the UnitInfos</returns>
+        Task<IEnumerable<UnitInfo>> GetUnitInfoAsync(string username);
+
+        /// <summary>
+        /// Creates the desired amount of units of the given type to the user.
+        /// </summary>
+        /// <param name="username">The name of the user</param>
+        /// <param name="unitId">The identifier of the unit</param>
+        /// <param name="count">The amount of units to be created</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the unitId is invalid</exception>
+        /// <exception cref="ArgumentException">Thrown when the count is not a valid amount</exception>
+        /// <returns>A UnitInfo containing the new amount of units</returns>
+        Task<UnitInfo> CreateUnitAsync(string username, int unitId, int count);
+
+        /// <summary>
+        /// Deletes the desired amount of units of the given type.
+        /// </summary>
+        /// <param name="username">The name of the user</param>
+        /// <param name="unitId">The identifier of the unit</param>
+        /// <param name="count">The amount of units to be deleted</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the unitId is invalid</exception>
+        /// <exception cref="ArgumentException">Thrown when the count is not a valid amount</exception>
+        /// <returns>A task that can be awaited</returns>
+        Task DeleteUnitsAsync(string username, int unitId, int count);
     }
 }
