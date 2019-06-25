0a76c9926244ad57f895096f65da6fa3eac37034
Péter Koppa <koppa96@windowslive.com>
2019-06-25 16:25:51 +0200

    Creating endpoint for report management

---
+++ backend/StrategyGame.Bll/DTO/Sent/CombatInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyGame.Bll.Dto.Sent
+{
+    public class CombatInfo
+    {
+        public int Id { get; set; }
+        public int Round { get; set; }
+        public bool IsAttack { get; set; }
+        public bool IsWon { get; set; }
+        public int EnemyCountryId { get; set; }
+        public string EnemyCountryName { get; set; }
+        public IEnumerable<UnitInfo> YourUnits { get; set; }
+        public IEnumerable<UnitInfo> EnemyUnits { get; set; }
+        public IEnumerable<UnitInfo> LostUnits { get; set; }
+        public int PealLoot { get; set; }
+        public int CoralLoot { get; set; }
+        public bool IsSeen { get; set; }
+    }
+}
---
+++ backend/StrategyGame.Bll/Services/Reports/IReportService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StrategyGame.Bll.Dto.Sent;
+
+namespace StrategyGame.Bll.Services.Reports
+{
+    /// <summary>
+    /// Service for managing the combat reports of a player.
+    /// </summary>
+    public interface IReportService
+    {
+        /// <summary>
+        /// Lists all the reports of a user personalized to them.
+        /// </summary>
+        /// <param name="username">The name of the user</param>
+        /// <returns>A sequence containing the reports</returns>
+        Task<IEnumerable<CombatInfo>> GetCombatInfoAsync(string username);
+
+        /// <summary>
+        /// Marks a report as seen for the user.
+        /// </summary>
+        /// <param name="username">The name of the user</param>
+        /// <param name="reportId">The identifier of the report</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the report id is not valid</exception>
+        /// <exception cref="UnauthorizedAccessException">Thrown when the user has no right to mark the report as seen</exception>
+        /// <returns>A task that can be awaited</returns>
+        Task SetSeenAsync(string username, int reportId);
+
+        /// <summary>
+        /// Marks a report as deleted for the user, and removes it from the database if both players marked it as deleted.
+        /// </summary>
+        /// <param name="username">The name of the user</param>
+        /// <param name="reportId">The identifier of the report</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the report id is not valid</exception>
+        /// <exception cref="UnauthorizedAccessException">Thrown when the user has no right to delete the report</exception>
+        /// <returns>A task that can be awaited</returns>
+        Task DeleteAsync(string username, int reportId);
+    }
+}
