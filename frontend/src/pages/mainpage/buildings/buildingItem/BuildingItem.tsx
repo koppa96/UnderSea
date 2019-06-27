1faabeb214bbb289f7d3efc88482f86d68dc6ff0
gszasza1 <gszasza1@gmail.com>
2019-06-27 16:51:39 +0200

    Begin rank

--- frontend/src/pages/mainpage/buildings/buildingItem/BuildingItem.tsx
+++ frontend/src/pages/mainpage/buildings/buildingItem/BuildingItem.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { BuildingProps } from "./Interface";
 import BuildingImg from "./../../../../assets/images/building-bg.png";
+import { BasePortUrl } from "../../../..";
 
 export const BuildingItem = (props: BuildingProps) => {
   const { title, description, price, amount, imageUrl } = props;
 
   return (
     <div className="building-item">
-      <img alt="building" src={BuildingImg} />
+      <img alt="building" src={BasePortUrl + props.imageUrl} />
       <div>
         <p>{title}</p>
         <p>{description}</p>
---
+++ frontend/src/pages/mainpage/rank/store/actions/RankAction.get.ts
@@ -0,0 +1,51 @@
+//ACTIONTYPES
+export interface IGetRankActionsTypes {
+  REQUEST: "RANK_REQUEST_GET_RANK";
+  SUCCES: "RANK_SUCCES_GET_RANK";
+  ERROR: "RANK_ERROR_GET_RANK";
+}
+
+export const GetRankActions: IGetRankActionsTypes = {
+  REQUEST: "RANK_REQUEST_GET_RANK",
+  SUCCES: "RANK_SUCCES_GET_RANK",
+  ERROR: "RANK_ERROR_GET_RANK"
+};
+
+export interface ISuccesParamState {
+  //Ranks: ICreationInfo[];
+}
+
+//ACTIONHOZ
+export interface IRequestActionGetRank {
+  type: IGetRankActionsTypes["REQUEST"];
+}
+export interface ISuccesActionGetRank {
+  type: IGetRankActionsTypes["SUCCES"];
+  params: ISuccesParamState;
+}
+export interface IErrorActionGetRank {
+  type: IGetRankActionsTypes["ERROR"];
+  params?: string;
+}
+
+//REDUCERHEZ
+export type IActions =
+  | IRequestActionGetRank
+  | ISuccesActionGetRank
+  | IErrorActionGetRank;
+
+//ACTIONCREATORHOZ
+export const GetRankActionCreator = (): IRequestActionGetRank => ({
+  type: GetRankActions.REQUEST
+});
+
+export const fetchError = (params?: string): IErrorActionGetRank => ({
+  type: GetRankActions.ERROR,
+  params
+});
+export const fetchSucces = (
+  params: ISuccesParamState
+): ISuccesActionGetRank => ({
+  type: GetRankActions.SUCCES,
+  params
+});
