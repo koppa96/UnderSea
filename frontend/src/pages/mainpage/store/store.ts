2737de2f94a576ccac83842b8cc2516d67351067
Krisz <szalkaititi@gmail.com>
2019-06-28 13:09:05 +0200

    loading state visual fix

--- frontend/src/pages/mainpage/store/store.ts
+++ frontend/src/pages/mainpage/store/store.ts
@@ -13,7 +13,20 @@ export interface MainpageResponseState {
 
 export const initialMainpageResponseState: MainpageResponseState = {
   error: undefined,
-  loading: false
+  loading: false,
+  model: {
+    round: 0,
+    rank: 0,
+    armyInfo: [],
+    pearls: 0,
+    corals: 0,
+    pearlsPerRound: 0,
+    coralsPerRound: 0,
+    event: undefined,
+    unseenReports: 0,
+    buildings: [],
+    researches: []
+  }
 };
 
 //REDUCER
