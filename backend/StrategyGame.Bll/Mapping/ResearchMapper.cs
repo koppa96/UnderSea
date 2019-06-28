e4a76cab1ee09234f8dddf381ddf406029bd9988
Péter Koppa <koppa96@windowslive.com>
2019-06-28 16:57:44 +0200

    Fixing some mappings

--- backend/StrategyGame.Bll/Mapping/ResearchMapper.cs
+++ backend/StrategyGame.Bll/Mapping/ResearchMapper.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using StrategyGame.Bll.Dto.Sent;
 using StrategyGame.Model.Entities;
+using System.Linq;
 
 namespace StrategyGame.Bll.Mapping
 {
@@ -15,8 +16,12 @@ namespace StrategyGame.Bll.Mapping
                 .ForMember(dest => dest.Name, conf => conf.MapFrom(src => src.Content.Name))
                 .ForMember(dest => dest.Description, conf => conf.MapFrom(src => src.Content.Description))
                 .ForMember(dest => dest.ImageUrl, conf => conf.MapFrom(src => src.Content.ImageUrl))
-                .ForMember(dest => dest.IconImageUrl, conf => conf.MapFrom(src => src.Content.IconImageUrl))
-                .ForMember(dest => dest.Cost, conf => conf.MapFrom(src => src.CostPearl));
+                .ForMember(dest => dest.Cost, conf => conf.MapFrom(src => src.Cost.Select(c => new ResourceInfo
+                {
+                    Name = c.ResourceType.Content.Name,
+                    Amount = (int)c.Amount,
+                    ImageUrl = c.ResourceType.Content.ImageUrl
+                })))
 
             CreateMap<ResearchType, BriefCreationInfo>()
                 .ForMember(dest => dest.ImageUrl, conf => conf.MapFrom(src => src.Content.ImageUrl))
