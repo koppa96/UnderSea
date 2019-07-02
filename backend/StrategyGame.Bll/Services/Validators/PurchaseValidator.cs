977b350440a5601a768bdef5ab3066a95f3bdf13
Péter Koppa <koppa96@windowslive.com>
2019-07-02 09:14:40 +0200

    Adding validation to PurchaseDetails

--- backend/StrategyGame.Bll/Services/Validators/PurchaseValidator.cs
+++ backend/StrategyGame.Bll/Services/Validators/PurchaseValidator.cs
@@ -1,19 +1,60 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
 using StrategyGame.Bll.DTO.Received;
+using StrategyGame.Dal;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
 
 namespace StrategyGame.Bll.Services.Validators
 {
-    public class PurchaseValidator : AbstractValidator<IEnumerable<PurchaseDetails>>
+    // List because IEnumerable doesn't work (It should)
+    public class PurchaseValidator : AbstractValidator<List<PurchaseDetails>>
     {
-        public PurchaseValidator()
+        private readonly string _user;
+        private readonly UnderSeaDatabaseContext _context;
+
+        public PurchaseValidator(IHttpContextAccessor contextAccessor, UnderSeaDatabaseContext context)
         {
-            RuleForEach(p => p).Must(p => BeGreaterThanZero(p.Count)).WithErrorCode("Invalid amount.");
+            _user = contextAccessor.HttpContext.User.Identity.Name;
+            _context = context;
+
+            RuleFor(p => p).MustAsync(HaveEnoughMoneyAsync)
+                .WithMessage("Not enough money.");
+
+            RuleFor(p => p)
+                .Must(pl => pl.All(p => p.Count > 0))
+                .WithMessage("Invalid amount.");
+                
+            RuleFor(p => p).MustAsync(UnitIdsExistAsync)
+                .WithMessage("Invalid unit id.");
         }
 
-        public bool BeGreaterThanZero(int amount)
+        public async Task<bool> UnitIdsExistAsync(List<PurchaseDetails> details, CancellationToken token = default)
         {
-            return amount > 0;
+            var unitIds = await _context.UnitTypes.Select(u => u.Id).ToListAsync();
+
+            foreach (var detail in details)
+            {
+                if (!unitIds.Any(u => u == detail.UnitId))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public async Task<bool> HaveEnoughMoneyAsync(List<PurchaseDetails> details, CancellationToken token = default)
+        {
+            var unitTypes = await _context.UnitTypes.ToListAsync();
+            var country = await _context.Countries.SingleAsync(c => c.ParentUser.UserName == _user);
+
+            int cost = details.Sum(d => d.Count * unitTypes.SingleOrDefault(u => u.Id == d.UnitId)?.CostPearl ?? 0);
+
+            return cost <= country.Pearls;
         }
     }
 }
