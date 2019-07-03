9be73512c0ee968874e73b05d7322e87ac2c0a75
Krisz <szalkaititi@gmail.com>
2019-07-03 12:04:50 +0200

    NotFound Page done

--- frontend/src/pages/notFound/NotFound.tsx
+++ frontend/src/pages/notFound/NotFound.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Wave from "./../../assets/images/wave.svg";
 
 export const NotFound = () => {
   return (
-    <>
-      <h3>Nyeh, mi az a gépelés</h3>
-      <span>Csak azért vagyok itt mert fragment</span>
-    </>
+    <div className="main-component notfound-width">
+      <div className="img-container">
+        <img className="wave-notfound" src={Wave} alt="wave" />
+        <h3 className="undersea-font-mainpage">UNDERSEA</h3>
+      </div>
+      <span className="not-found-text undersea-font">
+        Ajjaj, valami hiba történt!
+      </span>
+    </div>
   );
 };
