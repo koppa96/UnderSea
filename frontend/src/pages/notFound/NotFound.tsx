02c30abeee916300b93af72faff26ea513a991e4
Krisz <szalkaititi@gmail.com>
2019-07-03 13:31:28 +0200

    NotFound page redirect button

--- frontend/src/pages/notFound/NotFound.tsx
+++ frontend/src/pages/notFound/NotFound.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import Wave from "./../../assets/images/wave.svg";
+import { goBack } from "connected-react-router";
+import { Redirect } from "react-router";
 
-export const NotFound = () => {
-  return (
-    <div className="main-component notfound-width">
-      <div className="img-container">
-        <img className="wave-notfound" src={Wave} alt="wave" />
-        <h3 className="undersea-font-mainpage">UNDERSEA</h3>
+export class NotFound extends React.Component {
+  state = {
+    redirect: false
+  };
+  goBack = () => {
+    this.setState({ redirect: true });
+  };
+
+  render() {
+    console.log("redir: " + this.state.redirect);
+    return this.state.redirect ? (
+      <Redirect to="/" />
+    ) : (
+      <div className="main-component notfound-width">
+        <div className="img-container">
+          <img className="wave-notfound" src={Wave} alt="wave" />
+          <h3 className="undersea-font-mainpage">UNDERSEA</h3>
+        </div>
+        <span className="not-found-text undersea-font">
+          Ajjaj, valami hiba történt!
+        </span>
+        <button onClick={this.goBack} className="form-button">
+          Vissza a főoldalra
+        </button>
       </div>
-      <span className="not-found-text undersea-font">
-        Ajjaj, valami hiba történt!
-      </span>
-    </div>
-  );
-};
+    );
+  }
+}
